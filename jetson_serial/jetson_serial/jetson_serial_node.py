f26dc04bd221089ecebbf1b8cd3babb22e5389cd
Maanav Subramanian <msubram2@terpmail.umd.edu>
2026-04-27 18:25:28 -0400

    jetson to serial

---
+++ jetson_serial/jetson_serial/jetson_serial_node.py
@@ -0,0 +1,52 @@
+import rclpy
+from rclpy.node import Node
+import serial
+# Modify import based on actual message type
+# Using 64 bit float array for now
+from std_msgs.msg import Float64MultiArray
+
+class JetsonSerial(Node):
+    def __init__(self):
+            super().__init__('jetson_serial')
+            
+            # Subscriber Initialization
+            self.subscription = self.create_subscription(
+                  Float64MultiArray,
+                  'topic',
+                  self.listener_callback,
+                  10)
+            self.subscription
+            
+            # Serial Initialization
+            # Need to check the port and the baudrate
+            self.ser = serial.Serial(port = '/dev/ttyUSB0',
+                                     baudrate = 115200,
+                                     timeout = 1)
+
+    def listener_callback(self, msg):
+        data = msg.data
+
+        # Data length check in case it is imcomplete
+        if len(data) < 7:
+            self.get_logger().warn("Received Incomplete Message")
+            return
+        
+        # Format of string
+        str_format = "<" + ",".join([f"{x:.4f}" for x in data]) + ">"
+
+        # Writing String to Arduino
+        self.ser.write((str_format + "\n").encode('utf-8'))
+        self.get_logger().info(f"Sent: {str_format}")
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    node = JetsonSerial()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
+
+            
---
+++ jetson_serial/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'jetson_serial'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='maanav',
+    maintainer_email='msubram2@terpmail.umd.edu',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'jetson_serial_node = jetson_serial_node.py:main',
+        ],
+    },
+)
